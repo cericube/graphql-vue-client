56d68e38a367f861f134f275c2524d876c1c253b
cericube <cericube@users.noreply.github.com>
2025-09-10 16:16:04 +0900

    ch21

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp, h } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import { apolloClient } from "./apollo/client";
+import { DefaultApolloClient } from "@vue/apollo-composable";
+import { router } from "./router";
+
+const app = createApp({
+  setup() {
+    return () => h(App);
+  },
+});
+
+app.use(router);
+app.provide(DefaultApolloClient, apolloClient);
+app.mount("#app");
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,7 @@
+<!-- src/pages/HomePage.vue -->
+<template>
+  <div class="home">
+    <h1>GraphQL 클라이언트 실습 - 홈</h1>
+    <p>Vue Router와 Apollo Client 환경 구성이 완료되었습니다.</p>
+  </div>
+</template>
